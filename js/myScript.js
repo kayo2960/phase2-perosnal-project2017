ad27d152d9a18a43e778afc13133aa02b36c12ca
kayo2960 <cayokayo@gmail.com>
2017-07-24 15:47:12 +0000

    first commit

---
+++ js/myScript.js
@@ -0,0 +1,50 @@
+// JavaScript File
+/*global $*/ 
+
+function changeImage() {
+var image = document.getElementById('myImage');
+  if (image.src.match("tuna")) {
+    image.src="img/tunas.png";
+  }else {
+    image.src="img/tuna.png";
+ }
+}
+
+
+/*global google map js*/
+function initMap() {
+        // Create a map object and specify the DOM element for display.
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: { lat: 40.730610, lng: -73.935242 },
+          scrollwheel: false,
+          zoom: 10
+        });
+      }
+ 
+      
+// fish html
+$(document).ready(function(){
+  
+    /* $(function(){
+	$("button#fadeIn").click(function(){
+		$("img:not(:animated)").fadeIn("slow");
+	});
+	$("button#fadeOut").click(function(){
+		$("img:not(:animated)").fadeOut("slow");
+	});
+});*/
+  
+    $('#slide').click(function(){
+        $('.slide-box').effect('slide',{direction: 'right'}, 'slow');
+    });
+    
+    $(".slide-box").click(function(){
+        $(this).effect('explode',{pieces: 18},'slow');
+    });
+    
+  /*$('#show-text').click(function(){
+        $('#hidden-text').fadeToggle('slow')
+    });*/
+   
+});
+
