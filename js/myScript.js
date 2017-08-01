25721e5b5ecbb1e43cb70ca954754f0296ac411e
kayo2960 <cayokayo@gmail.com>
2017-08-01 14:53:06 +0000

    latest update 8/1

--- js/myScript.js
+++ js/myScript.js
@@ -32,11 +32,11 @@ $(document).ready(function(){
 /*global onload*/
 /*/////////////////animation/////////////////////////////*/
 
-$('.box1').animate({left: 400}, 1000);
+$('.box1').animate({left: 200}, 1000);
 // Chained animation
-$('.box2').animate({ 
+$('.box2').animate({left: 200, 
     top: 100,
-    left: 100,
+    left: 300,
     width: 100,
     height: 100,
 }).animate({
@@ -48,15 +48,68 @@ $('.box2').animate({
     top: 150,
     left: 300,
     width: 400,
-    height: 120,
+    height: 20,
 }).animate({
     top: 70,
     left: 10,
     width: 50,
-    height: 50,
+    height: 70,
 });
 
 });
 
 
+/*==============FORM PAGE =======================================================*/
+/*(function(){
+            //'use strict';
+          
+            var slider = document.getElementById('slider');
+            var label = document.getElementById('label');
+            var btn = document.getElementById('btn');
+            var result = document.getElementById('result');
+            var numbers = document.getElementById('numbers');
+            var symbols = document.getElementById('symbols');
+            
+            function getPassword(){
+                var seed_letters = 'abcdefghijklmnopqrstuvwzyx';
+                var seed_numbers = '`123456789';
+                var seed_symbols = '!@#$%^&*()_+';
+                var seed;
+                
+                var len = slider.value;
+                var pwd = '';
+                
+                seed = seed_letters + seed_letters.toUpperCase();
+                if (numbers.checked === true) {
+                    seed += seed_numbers;
+                }
+                
+                if (symbols.checked) {
+                    seed += seed_numbers;
+                }
+                
+                
+               // for (var i = 0; i < len; i++){
+                 //   pwd += seed[Math.floor(Math.random() * seed.length)];
+               // }
+              //   result.value = 'fjkdjfjkasjdfk';
+            }
+            
+                    slider.addEventListener('change',function() {
+                        label.innerHTML = this.value;
+                    });
 
+                    btn.addEventListener('click',function() {
+                        getPassword();
+                       // result.value = 'fjkdjfjkasjdfk';
+                    });
+
+                    result.addEventListener('click',function() {
+                        this.select();
+                        
+                        getPassword();//uchigawa ni ireta kore
+                    });
+
+                    
+            
+        })();*/
